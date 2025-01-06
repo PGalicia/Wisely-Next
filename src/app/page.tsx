26a14c27d11f85d36eb18acce395ee022c7a4bb3
Patrick Galicia <galicia.patrick@gmail.com>
2025-01-06 14:27:59 -0800

    Update ts ignore comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,19 @@ import { withPageAuthRequired } from '@auth0/nextjs-auth0';
 // Components
 import MainHome from '@/components/MainHome';
 
-// @ts-ignore
+/**
+ * Reasoning for ts-ignore below:
+ * 
+ * There is typing conflict with PageProps and arguments for withPageAuthRequired.
+ * That comment is needed to ignore those error because it works fine with the typing error.
+ * It's possible since NextJs with App router is still new, this error hasn't been fixed with
+ * the nextjs-auth0 library
+ */
+
+// @ts-ignore: Check above for reasoning
 async function HomePage() {
   return <MainHome />;
 }
 
-// @ts-ignore
+// @ts-ignore: Check above for reasoning
 export default withPageAuthRequired(HomePage);
