11675158d166dccb0f085af4d1655e5e975d73a2
Patrick Galicia <galicia.patrick@gmail.com>
2025-01-06 13:47:51 -0800

    Update props typing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
  */
 
 // Auth0
-import { withPageAuthRequired, AppRouterPageRoute } from '@auth0/nextjs-auth0';
+import { withPageAuthRequired, AppRouterPageRouteOpts } from '@auth0/nextjs-auth0';
 
 // Components
 import MainHome from '@/components/MainHome';
@@ -11,8 +11,8 @@ import MainHome from '@/components/MainHome';
 // Types
 import type React from 'react';
 
-async function HomePage() {
+async function HomePage({}: AppRouterPageRouteOpts) {
   return <MainHome />;
 }
 
-export default withPageAuthRequired(HomePage as AppRouterPageRoute);
+export default withPageAuthRequired(HomePage);
